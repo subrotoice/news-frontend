12bae68c08baecfa1952d43042db705fe9679677
Subroto Biswas <subroto.iu@gmail.com>
2024-12-06 10:57:52 +0600

    Fix: daisyui in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -10,5 +11,5 @@ export default {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
